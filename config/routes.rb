1e1fda8befdc24478b95b5cb9c694d351a6ee7df
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-04-29 21:29:16 -0700

    Adding pages controller, setting up homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get 'pages/about', to: 'pages#about'
+  get 'pages/home', to: 'pages#home'
 end
