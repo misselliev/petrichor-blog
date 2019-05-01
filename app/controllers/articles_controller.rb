48af843c28ac0e2a06d08e5a4284bd8db7e731a6
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-04-30 17:28:29 -0700

    Adding UI for new and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,10 +6,17 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = 'Article was created!'
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
