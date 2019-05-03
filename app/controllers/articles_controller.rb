7a13121dd94aeeccbed147b0d0351be20dd0b637
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-05-03 11:45:02 -0700

    Adding edit and update to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,20 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+  end
+end
+
   def create
     @article = Article.new(article_params)
     if @article.save
