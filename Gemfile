f19efa44c917844f16e9ecfff7ac910bc1a41a28
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-05-23 09:08:08 -0700

    styling and building home

--- Gemfile
+++ Gemfile
@@ -10,8 +10,10 @@ gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
+gem 'bootstrap-sass', '~> 3.4.1'
+gem 'sassc-rails', '>= 2.1.0'
 gem 'jquery-rails'
-gem 'bootstrap-sass'
+gem 'rails-ujs'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require bootstrap 
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require rails-ujs 
-//= require jquery 
-//= require bootstrap-sprockets 
+//= require popper 
 
