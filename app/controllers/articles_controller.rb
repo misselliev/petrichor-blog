2ef87f357778692ee47fe3a3706783ef82abddcb
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-06-07 11:06:20 -0700

    styling flash partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = 'Article was updated'
+      flash[:success] = 'Article was updated'
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = 'Article was created!'
+      flash[:success] = 'Article was created!'
       redirect_to article_path(@article)
     else
       render 'new'
@@ -35,7 +35,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = 'Article was deleted'
+    flash[:danger] = 'Article was deleted'
     redirect_to articles_path
   end
 
