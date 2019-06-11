25cda4fcab9ee6eb8e47272e42fc7d575b534cfb
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-06-11 16:26:26 -0700

    login form added, implementing sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'You have logged in!'
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'There was something wrong with your information'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success]="You logged out! See you soon!"
+  end
+end
