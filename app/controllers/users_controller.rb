b9f72788f43bb78526c0234cd57d2572ddf4120c
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-06-11 17:57:39 -0700

    restricting user profile editing to their own profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 
 class UsersController < ApplicationController
   before_action :set_user, only: %i[edit update show]
-
+  before_action :require_same_user, only: %i[edit update]
   def index
     @users = User.paginate(page: params[:page], per_page: 3)
   end
@@ -44,5 +44,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = 'You can only edit your own account'
+      redirect_to root_path
+    end
   end
 end
