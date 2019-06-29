c4ac5d8f4ad6b5dea5f21f38647192b41366c62d
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-06-28 17:35:24 -0700

    adding associations for articles categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
+  has_many :articles_categories
+  has_many :articles, through: :articles_categories
+  
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_221244) do
+ActiveRecord::Schema.define(version: 2019_06_28_233741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
