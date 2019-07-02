adab0adb0837244838515dd337d9f3c79b8d7a25
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-07-02 10:50:01 -0700

    updating views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CategoriesController < ApplicationController
-  before_action :require_admin, except: [:index, :show]
+  before_action :require_admin, except: %i[index show]
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
@@ -20,7 +20,10 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def show; end
+  def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
+  end
 
   private
 
@@ -29,8 +32,8 @@ class CategoriesController < ApplicationController
   end
 
   def require_admin
-    if !logged_in? || (logged_in? and !current_user.admin?)
-      flash[:danger]="Only admins can perform that action"
+    if !logged_in? || (logged_in? && !current_user.admin?)
+      flash[:danger] = 'Only admins can perform that action'
       redirect_to categories_path
     end
   end
