a3449de3628a483b0b71ad292a9e870096881956
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-07-02 19:12:23 -0700

    Update all views with categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  has_many :articles_categories
-  has_many :articles, through: :articles_categories
+  has_many :article_categories
+  has_many :articles, through: :article_categories
   
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
